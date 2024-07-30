178b8413e924e003e622d0df7d1977db5b675182
Fabian Bull <spam+github@fbull.de>
2024-07-30 11:02:39 +0200

    Stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
-// Uncomment this block to pass the first stage
-use std::{net::TcpListener, io::Write};
+use std::{
+    io::{BufReader, Write, BufRead},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream);
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
 
-     for stream in listener.incoming() {
-         match stream {
-             Ok(mut stream) => {
-                 stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
-                 println!("accepted new connection");
-             }
-             Err(e) => {
-                 println!("error: {}", e);
-             }
-         }
-     }
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let request_line = reader.lines().next().unwrap().unwrap();
+
+    let response = match request_line.as_str() {
+        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+    stream.write_all(response.as_bytes()).unwrap();
 }
